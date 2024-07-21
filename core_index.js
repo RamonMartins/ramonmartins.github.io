673a9c06accd0b3a3d5584d79577f71d05d59d73
Ramon Martins <ramon-bac@hotmail.com>
2024-07-21 00:59:51 -0300

    new

--- core_index.js
+++ core_index.js
@@ -7,9 +7,9 @@ function ResetarCampos(){
 }
 
 function EnviarDados(){
-    let inputs = document.getElementsByClassName("inputs")
-    let v_inp_1 = document.getElementById("input1").value
-    let v_inp_2 = document.getElementById("input2").value
+    let v_inp_1 = document.getElementById("info_nome").value
+    let v_inp_2 = document.getElementById("info_email").value
+    let v_inp_3 = document.getElementById("info_curso").value
 
-    window.location.href = "painel_estudante.htm?nome=" + encodeURIComponent(v_inp_1) + "&curso=" + encodeURIComponent(v_inp_2)
+    window.location.href = "painel_estudante.htm?nome=" + encodeURIComponent(v_inp_1) + "&email=" + encodeURIComponent(v_inp_2) + "&curso=" + encodeURIComponent(v_inp_3)
 }
--- core_painel.js
+++ core_painel.js
@@ -1,16 +1,69 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Coletar dados vindos da página index
     let parametros = new URLSearchParams(window.location.search)
     let nome = parametros.get("nome").toUpperCase()
+    let email = parametros.get("email")
     let curso = parametros.get("curso").toUpperCase()
-    console.log(nome)
-    console.log(curso)
     
+
+    // Coletar datas atuais para usos posteriores
+    let data_atual = new Date()
+    let ano_atual = data_atual.getFullYear()
+    let mes_atual = data_atual.getMonth()
     
-    let busca_nomes = document.getElementsByClassName("nome-user-trocar")
+
+    // Alterar o nome em mais de um lugar
+    let busca_nomes = document.getElementsByClassName("info_nome")
     for(let x of busca_nomes){
         x.textContent = nome
     }
+
+
+    // Alterar o Prédio do curso
+    let busca_predio = document.getElementById("info_predio")
+    let predio_CCHL = ("CCHL - ADMINISTRAÇÃO", "CCHL - DIREITO")
+    let predio_CCN = ("CCN - CIÊNCIA DA COMPUTAÇÃO", "CCN - FÍSICA", "CCN - MATEMÁTICA")
+    let predio_CT = ("CT - ENGENHARIA CIVIL", "CT - ENGENHARIA DE PRODUÇÃO", "CT - ENGENHARIA ELÉTRICA")
+
+    if(predio_CCHL.includes(curso)){
+        busca_predio.textContent = "CENTRO DE CIENCIAS HUMANAS E LETRAS"
+    }else if(predio_CCN.includes(curso)){
+        busca_predio.textContent = "CENTRO DE CIENCIAS DA NATUREZA"
+    }else if(predio_CT.includes(curso)){
+        busca_predio.textContent = "CENTRO DE TECNOLOGIA"
+    }else{
+        busca_predio.textContent = "CENTRO DE CIENCIAS DA NATUREZA"
+    }
+
+
+    // Alterar Semestre atual
+    let busca_semestre = document.getElementById("info_semestre")
+    /* Contagem dos meses no JavaScript começa em 0 e nao 1,
+        então o intervalo de meses é de 0 - 11 */
+    if(mes_atual < 6){
+        busca_semestre.textContent = ano_atual + "." + 1
+    }else{
+        busca_semestre.textContent = ano_atual + "." + 2
+    }
     
-    let busca_curso = document.getElementById("curso-user-trocar")
+
+    // Alterar nº matrícula
+    let busca_matricula = document.getElementById("info_matricula")
+    busca_matricula.textContent = (ano_atual - 1).toString() + Math.floor(100000 + Math.random() * 900000)
+    
+
+    // Alterar o curso
+    let busca_curso = document.getElementById("info_curso")
     busca_curso.textContent = curso
+
+
+    // Alterar o e-mail
+    let busca_email = document.getElementById("info_email")
+    busca_email.textContent = email
+
+
+    // Alterar data de entrada no curso
+    let busca_entrada = document.getElementById("info_entrada")
+    busca_entrada.textContent = ano_atual - 1
+    
 });
